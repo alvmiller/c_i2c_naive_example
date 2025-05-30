6ac2b0997eedc1b63581d1659302900dbec7a143
alvmiller <134068320+alvmiller@users.noreply.github.com>
2025-05-30 23:45:31 +0300

    Update i2c_example.c

--- i2c_example.c
+++ i2c_example.c
@@ -31,8 +31,15 @@
 // https://eraretuya.github.io/2016/12/10/the-i2c-stub-in-action/
 // https://hackaday.io/project/178271-i2c-master-mode-emulator
 
+/*****************************************************************************************/
+// sudo modprobe i2c-dev
+// sudo modprobe i2c-stub chip_addr=0x03
+// i2cdetect -l
+// i2c-1	unknown   	SMBus stub driver               	N/A
+
 // gcc -Wall -std=gnu11 -ggdb i2c_example.c
 // sudo ./a.out
+/*****************************************************************************************/
 
 // ls /sys/bus/i2c/drivers/<TBD>
 // echo <TBD>-<TBD> > /sys/bus/i2c/drivers/<TBD>/unbind
@@ -45,7 +52,7 @@
 // i2cset	Set I2C registers on a connected device with new data or values.
 // i2ctransfer	Send user-defined I2C messages in one transfer to a connected device.
 
-static inline int32_t i2c_smbus_access(
+static inline int32_t i2c_access(
 	int file,
 	char read_write,
 	uint8_t command,
@@ -61,15 +68,14 @@ static inline int32_t i2c_smbus_access(
 }
 
 
-static inline int32_t i2c_smbus_read_byte_data(
+static inline int32_t i2c_read_byte(
 	int file,
 	uint8_t command)
 {
 	union i2c_smbus_data data = {};
-	int32_t res = i2c_smbus_access(file, I2C_SMBUS_READ, command, I2C_SMBUS_BYTE_DATA, &data);
-	//int32_t res = i2c_smbus_access(file, I2C_SMBUS_READ, command, I2C_SMBUS_BYTE_DATA, &data);
+	int32_t res = i2c_access(file, I2C_SMBUS_READ, command, I2C_SMBUS_BYTE_DATA, &data);
 	if (res != 0) {
-		//err(errno, "Access failed: %d", res);
+		err(errno, "Access failed: %d", res);
 		return -1;
 	}
 	else {
@@ -83,13 +89,16 @@ int main(int argc, char **argv)
 	(void)argv;
 
 	uint8_t data = 0;
-	//const uint8_t addr = 0x76;
-	const uint8_t addr = 0x4a;
-	//const uint8_t reg = 0x0d;
+	const uint8_t addr = 0x3;
 	const uint8_t reg = 0;
-	const char *path = "/dev/i2c-0";
+	const char *path = "/dev/i2c-1";
 	int file = -1;
 	int rc = 0;
+
+	//const uint8_t addr = 0x76;
+	//const uint8_t addr = 0x4a;
+	//const uint8_t reg = 0x0d;
+	//const char *path = "/dev/i2c-0";
 
 	file = open(path, O_RDWR);
 	if (file < 0) {
@@ -102,8 +111,8 @@ int main(int argc, char **argv)
 		err(errno, "Cannot set device address '0x%02x'", addr);
 	}
 
-	data = i2c_smbus_read_byte_data(file, reg);
-	printf("%s: device 0x%02x at address 0x%02x: 0x%02x\n",
+	data = i2c_read_byte(file, reg);
+	printf("Device %s at address 0x%02x with command 0x%02x: 0x%02x\n",
 		path, addr, reg, data);
 
 	return 0;
